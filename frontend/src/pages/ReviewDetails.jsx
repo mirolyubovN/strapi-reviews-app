a59609a80d6e8361540a01804e141019fbdb0cb2
Nikita Mirolyubov <mirolyubov_n@yahoo.co.uk>
2023-02-17 09:25:35 +0600

    fetch single record

--- frontend/src/pages/ReviewDetails.jsx
+++ frontend/src/pages/ReviewDetails.jsx
@@ -1,3 +1,26 @@
+import { useParams } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
+
 export default function ReviewDetails() {
-  return <div>Review Details</div>;
+
+  const { id } = useParams();
+
+  const { data, loading, error } = useFetch('http://localhost:1337/api/reviews/' + id);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
+  return (
+    <div className='review-card'>
+      <h2 className='review-card__title'>
+        {data.attributes.title}
+      </h2>
+      <div className='review-card__rating'>
+        {data.attributes.rating}
+      </div>
+      <div className='review-card__body'>
+        {data.attributes.body}
+      </div>
+    </div>
+  );
 }
